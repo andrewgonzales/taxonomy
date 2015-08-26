91bdfdd4a7e4827b5b9f10d954e4dc3bb3097d68
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-26 14:51:14 -0700

    Completed taxonomy

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,10 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
+HoneyMakerBee.prototype.eat = Grub.prototype.eat;
+HoneyMakerBee.prototype.makeHoney = function(){this.honeyPot++};
+HoneyMakerBee.prototype.giveHoney = function(){this.honeyPot--};
 
